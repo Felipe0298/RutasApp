e6ce317bd9ed145da83b3c4da7e10d9b92a68657
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-14 13:10:42 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
+
+export const App = () => {
+  return (
+    <View>
+      <Icon
+      name='star-outline'
+      color='red'
+      size={50} 
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
